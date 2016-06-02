6994916c33b91be27f365629d3df07a157e56b46
jescoyle <jrcoyle@live.unc.edu>
2016-06-02 09:19:29 -0700

    added new script for making manuscript figures that summarize across several runs

---
+++ R/make_paperfigures.R
@@ -0,0 +1,428 @@
+## This script generates figures for the CAMM Manuscript
+
+
+options(stringsAsFactors=F)
+library(reshape2)
+library(lattice) # Plots
+library(latticeExtra)
+library(MBESS) # confidence intervals on Cohen's d
+
+# Main directory
+working_dir = 'C:/Users/jrcoyle/Documents/Research/CAMM/'
+setwd(working_dir)
+
+# Directory for saving figures
+fig_dir = 'C:/Users/jrcoyle/Documents/Research/CAMM/Figures/Manuscript/'
+
+# Load functions
+code_dir = 'C:/Users/jrcoyle/Documents/Research/CAMM/GitHub/CAMM/R/'
+git_dir = 'C:/Users/jrcoyle/Documents/Research/CAMM/GitHub/CAMM/'
+source(paste(code_dir,'analysis_functions.R', sep=''))
+source(paste(code_dir,'simulation_functions.R', sep=''))
+
+# Load commununity and correlation summaries from runs
+comm1 = read.csv(paste0(git_dir, 'Results/comm_summary_run1.csv'))
+cor1 = read.csv(paste0(git_dir, 'Results/cor_summary_run1.csv'))
+comm2 = read.csv(paste0(git_dir, 'Results/comm_summary_run2.csv'))
+cor2 = read.csv(paste0(git_dir, 'Results/cor_summary_run2.csv'))
+
+## Define symbols used for plotting throughout
+a_pch = c(16, 1)
+b_pch = c(15, 0)
+n_pch = c(17, 2)
+topo_pch = c(15,16,17); names(topo_pch) = c('one2one','one2many','many2many')
+
+use_stats= c('S_a','S_b','Beta_a','Beta_b','N','Cor_ab')
+stat_names = expression(S[host],S[sym],beta[host],beta[sym],N,S[host]%~%S[sym]); names(stat_names)=use_stats
+stat_pch = c(0,1,2,4,5,6); names(stat_pch) = use_stats
+
+use_cor = c('cor_a_1','cor_a_2','cor_b_1','cor_b_2')
+cor_names = expression(Host%~%E[1], Host%~%E[2], Sym%~%E[1], Sym%~%E[2])
+cor_S_names = expression(S[host]%~%E[1], S[host]%~%E[2], S[sym]%~%E[1], S[sym]%~%E[2])
+cor_pch = c(a_pch, b_pch); names(cor_pch) = use_cor
+
+topo_names = c('Both Specialist', 'Host Specialist', 'Both Non-Specialist'); names(topo_names) = c('one2one','one2many','many2many')
+env_names = c('None','Same Gradient','Opposite Gradients','Both Gradients'); names(env_names) = c('none','same','opposite','all')
+
+##################################################################
+## Functions
+
+# A function that calculates standardized mean difference (Cohen's d)
+# m : a vector of length 2 with the sample means
+# n : a vector of length 2 with the sample standard deviations
+# n : a vector of length 2 with the sample sizes
+# use.s : character indicating whether the standard deviation used should be 'pooled', 's1', or 's2'
+
+calc_d = function(m, s, n, use.s='pooled'){
+
+	if(use.s=='pooled'){
+		s = sqrt(((n[1]-1)*s[1]^2 + (n[2]-1)*s[2]^2)/(sum(n)+2))	
+	}
+	if(use.s=='s1'){
+		s = s[1]
+	}
+	if(use.s=='s2'){
+		s = s[2]
+	}
+	
+	(m[2]- m[1])/s	
+}
+
+
+##################################################################
+## Experiment 1: Degree of Mutualistic Dependence
+
+## Figure 1
+## To show no effect of weak facultative mutualism: Effects size plot of 
+## (A) all community stats and (B) community correlations from omega=0 baseline (from RUN1 with mort rates at 10) regressed on omega.
+
+omegas = seq(0,1,.1)
+
+# A) Community statistics
+plot_data = subset(comm1, mra==10 & mrb==10 & summary=='mean')
+plot_data = melt(plot_data, id.vars=c('o','stat'), measure.vars=6:14)
+plot_data = acast(plot_data, o~stat~variable)
+
+# Calculate standardized mean difference (Cohen's d)
+# Use both pooled and s1 for variance
+eff_data = sapply(use_stats, function(y){
+	as.numeric(sapply(omegas, function(o){
+		calc_d(plot_data[c('0',o),'mean',y], sqrt(plot_data[c('0',o),'var',y]), c(100,100), use.s='pooled')
+	}))
+})
+
+# Calculate CI on Cohen's d (standardized mean difference), assumes equal variance
+CIs = apply(eff_data, c(1,2), function(x){
+	ci = ci.smd(smd=x, n.1=100, n.2=100, conf.level=0.95)
+	as.numeric(ci)
+})
+
+#svg(paste0(fig_dir,'Effect of mutualism strength on community statistics pooled.svg'), height=4.5, width=5)
+par(mar=c(5,4,1,4))
+par(lend=1)
+plot(omegas, rep(0,11), type='n', ylim=range(CIs), axes=F,
+	xlab=expression(Strength~~of~~mutualism~~(omega)), ylab='Effects Size (d)')
+abline(h=0, col='grey50', lty=2, lend=1)
+for(stat in use_stats){
+	polygon(c(omegas,rev(omegas)), c(CIs[1,,stat],CIs[3,11:1,stat]), col='#00000020', border=NA)	
+	points(omegas, eff_data[,stat], pch=stat_pch[stat])
+	lines(omegas, eff_data[,stat])
+}
+par(xpd=T)
+text(rep(1, length(use_stats)), eff_data[11,], stat_names, pos=4)
+par(xpd=F)
+#legend('bottomleft', stat_names, pch=use_pch, bty='n')
+axis(1)
+axis(2, las=1)
+abline(h=par('usr')[3], lwd=3)
+abline(v=par('usr')[1], lwd=3)
+#dev.off()
+
+# B) Correlations
+plot_data = subset(cor1, mra==10 & mrb==10 & summary=='mean')
+plot_data = melt(plot_data, id.vars=c('o','stat','env','measure'), measure.vars=c('cor_a','cor_b'))
+plot_data = acast(plot_data, o ~ stat ~ measure ~ variable + env)
+
+# Calculate standardized mean difference (Cohen's d)
+# Use both pooled and s1 for variance
+eff_data = apply(plot_data, c(3,4), function(y){
+	as.numeric(sapply(omegas, function(o){
+		calc_d(y[c('0',o),'mean'], sqrt(y[c('0',o),'var']), c(100,100), use.s='s1')
+	}))
+})
+
+# Calculate CI on Cohen's d (standardized mean difference), assumes equal variance
+CIs = apply(eff_data, c(1,2,3), function(x){
+	ci = ci.smd(smd=x, n.1=100, n.2=100, conf.level=0.95)
+	as.numeric(ci)
+})
+
+
+# Community composition
+#svg(paste0(fig_dir,'Effect of mutualism strength on community composition correlation s1.svg'), height=4.5, width=5)
+par(mar=c(5,4,1,4))
+par(lend=1)
+plot(omegas, rep(0,11), type='n', ylim=range(CIs[,,'rda',]), axes=F,
+	xlab=expression(Strength~~of~~mutualism~~(omega)), ylab='Std. mean difference (d)')
+abline(h=0, col='grey50', lty=2, lend=1)
+for(stat in use_cor){
+	polygon(c(omegas,rev(omegas)), c(CIs[1,,'rda',stat],CIs[3,11:1,'rda',stat]), col='#00000020', border=NA)	
+	points(omegas, eff_data[,'rda',stat], pch=cor_pch[stat])
+	lines(omegas, eff_data[,'rda',stat])
+}
+legend('topleft', cor_names, pch=cor_pch, bty='n', title=expression(RDA~~R^2), ncol=2)
+axis(1)
+axis(2, las=1)
+abline(h=par('usr')[3], lwd=3)
+abline(v=par('usr')[1], lwd=3)
+#dev.off()
+
+# Species richness
+#svg(paste0(fig_dir,'Effect of mutualism strength on species richness correlation s1.svg'), height=4.5, width=5)
+par(mar=c(5,4,1,4))
+par(lend=1)
+plot(omegas, rep(0,11), type='n', ylim=range(CIs[,,c('rda','S'),]), axes=F,
+	xlab=expression(Strength~~of~~mutualism~~(omega)), ylab='Std. mean difference (d)')
+abline(h=0, col='grey50', lty=2, lend=1)
+for(stat in use_cor){
+	polygon(c(omegas,rev(omegas)), c(CIs[1,,'S',stat],CIs[3,11:1,'S',stat]), col='#00000020', border=NA)	
+	points(omegas, eff_data[,'S',stat], pch=cor_pch[stat])
+	lines(omegas, eff_data[,'S',stat])
+}
+legend('topleft', cor_S_names, pch=cor_pch, bty='n', title=expression(Pearson~~italic(r)), ncol=2)
+axis(1)
+axis(2, las=1)
+abline(h=par('usr')[3], lwd=3)
+abline(v=par('usr')[1], lwd=3)
+#dev.off()
+
+
+## Figure 2
+## To show interaction with topology and type of filtering: Effects size (from o=0) of 
+## (A) (some) community stats and (B) community correlations under different topology and filtering (from RUN 2).
+
+omegas = c(0,0.5,0.9,1)
+envfilt = c('none','same','opposite','all')
+topos = c('one2one','one2many','many2many')
+
+## A) Community statistics
+plot_data = subset(comm2, summary=='mean')
+plot_data = melt(plot_data, id.vars=c('o','topo','envfilt','stat'), measure.vars=6:14)
+plot_data = acast(plot_data, o~topo~envfilt~stat~variable)
+
+# Calculate standardized mean difference (Cohen's d)
+# Use both pooled and s1 for variance
+eff_data = apply(plot_data[,,,,use_stats], c(2,3,5), function(y){
+	as.numeric(sapply(rownames(y), function(o){
+		calc_d(y[c('0',o),'mean'], sqrt(y[c('0',o),'var']), c(100,100), use.s='pooled')
+	}))
+})
+
+# Calculate CI on Cohen's d (standardized mean difference), assumes equal variance
+CIs = apply(eff_data, c(1,2,3,4), function(x){
+	if(is.na(x)){
+		rep(NA,3)
+	} else {
+		as.numeric(ci.smd(smd=x, n.1=100, n.2=100, conf.level=0.95))
+	}
+})
+
+
+# Cor a ~ b is NA for topo = many2many and envfilt = none because S_b = 10 in all communities for several chains
+# May want to re-write summary code so that NAs don't propagate, but will wait to see how pervasive this problem is.
+
+#svg(paste0(fig_dir, 'Effect of mutualism strength on community stats across topo and envfilt (pooled).svg'))
+par(mar=c(.5,.5,.5,.5))
+par(oma=c(4,4,3,0))
+layout(matrix(c(1:12, rep(13,4)), nrow=4))
+par(lend=1)
+
+for(n in topos){
+for(f in envfilt){
+
+plot(omegas, rep(0,4), type='n', ylim=range(CIs, na.rm=T), axes=F,
+	xlab='', ylab='')
+abline(h=0, col='grey50', lty=2, lend=1)
+for(stat in use_stats){
+	not_missing = which(!is.na(eff_data[,n,f,stat]))
+	use_x = omegas[not_missing]
+	polygon(c(use_x,rev(use_x)), c(CIs[1,not_missing,n,f,stat],CIs[3,rev(not_missing),n,f,stat]), col='#00000020', border=NA)	
+	points(use_x, eff_data[not_missing,n,f,stat], pch=stat_pch[stat])
+	lines(use_x, eff_data[not_missing,n,f,stat])
+}
+# Add labels
+if(f == 'all') axis(1)
+if(n == 'one2one') axis(2, las=1)
+if(f == 'none') mtext(topo_names[n], 3, 1, cex=.8)
+if(n == 'many2many'){
+	par(xpd=NA)
+	text(1, 1 , env_names[f], srt=-90, cex=1.3, adj=c(.5,-2))
+	par(xpd=F)
+}
+	
+# Add axis lines
+abline(h=par('usr')[3], lwd=3)
+abline(v=par('usr')[1], lwd=3)
+
+
+}} # closes for loops
+
+mtext(expression(Strength~~of~~mutualism~~(omega)), 1, 2.5, cex=.8, outer=T, adj=0.35)
+mtext('Std. mean difference', 2, 2.5, cex=.8, outer=T)
+
+plot.new()
+legend('center', stat_names, pch=use_pch, bty='n', cex=1.2)
+
+#dev.off()
+
+
+## Plot only for obligate mutualism
+
+jit = c(-.1,0,.1); names(jit) = topos
+x = 1:length(use_stats)
+
+#svg(paste0(fig_dir,'Effects of obligate mutualism on community stats across envfilt and topo (pooled).svg'), height=9, width=3.5)
+par(mfrow=c(4,1))
+par(mar=c(3,4,2,.5))
+par(lend=1)
+for(f in envfilt){
+	
+	plot(x, rep(0, length(use_stats)), type='n', xlim=c(0.5,length(use_stats)+.5),
+		ylim=range(CIs[,4,,,], na.rm=T),axes=F, ylab='Std. mean difference', xlab='')
+	
+	# Add axis lines
+	axis(1, at=x, labels=stat_names, tick=F, padj=0)
+	axis(2, las=1)
+	#abline(v=x+.5, lty=1, col='grey80')
+	abline(h=seq(-4,2,2), lty=2, col='grey')
+	abline(h=par('usr')[3], lwd=3)
+	abline(v=par('usr')[1], lwd=3)
+
+	# Add points
+	for(n in topos){
+		xvals = jit[n] + x
+		arrows(xvals, CIs[1,4,n,f,], xvals, CIs[3,4,n,f,], angle=90, length=.03, code=3)
+		points(xvals, eff_data[4,n,f,use_stats], pch=topo_pch[n])
+	}
+
+	# Add plot label
+	plot_lab = paste0(letters[which(envfilt==f)], ') ', env_names[f])
+	mtext(plot_lab, 3, at=-1, line=0.5, adj=0, cex=.8)
+
+	# Add legend to first plot
+	if(f == 'none') legend('topright', topo_names[topos], pch=topo_pch[topos], border='grey', bg='white')
+
+}
+#dev.off()
+
+
+## B) Community correlations
+plot_data = subset(cor2, summary=='mean')
+plot_data = melt(plot_data, id.vars=c('o','topo','envfilt','stat','env','measure'), measure.vars=c('cor_a','cor_b'))
+plot_data = acast(plot_data, o ~ topo~envfilt ~ stat ~ measure ~ variable + env)
+
+# Calculate standardized mean difference (Cohen's d)
+# Use both pooled and s1 for variance
+eff_data = apply(plot_data, c(2,3,5,6), function(y){
+	as.numeric(sapply(omegas, function(o){
+		calc_d(y[c('0',o),'mean'], sqrt(y[c('0',o),'var']), c(100,100), use.s='pooled')
+	}))
+})
+
+# Calculate CI on Cohen's d (standardized mean difference), assumes equal variance
+CIs = apply(eff_data, 1:5, function(x){
+	if(is.na(x)){
+		rep(NA,3)
+	} else {
+		as.numeric(ci.smd(smd=x, n.1=100, n.2=100, conf.level=0.95))
+	}
+})
+
+svg(paste0(fig_dir, 'Effect of mutualism strength on community correlations across topo and envfilt (pooled).svg'))
+par(mar=c(.5,.5,.5,.5))
+par(oma=c(4,4,3,0))
+layout(matrix(c(1:12, rep(13,4)), nrow=4))
+par(lend=1)
+
+for(n in topos){
+for(f in envfilt){
+
+plot(omegas, rep(0,4), type='n', ylim=range(CIs[,,,,'rda',], na.rm=T), axes=F,
+	xlab='', ylab='')
+abline(h=0, col='grey50', lty=2, lend=1)
+for(stat in use_cor){
+	not_missing = which(!is.na(eff_data[,n,f,'rda',stat]))
+	use_x = omegas[not_missing]
+	polygon(c(use_x,rev(use_x)), c(CIs[1,not_missing,n,f,'rda',stat],CIs[3,rev(not_missing),n,f,'rda',stat]), col='#00000020', border=NA)	
+	points(use_x, eff_data[not_missing,n,f,'rda',stat], pch=cor_pch[stat])
+	lines(use_x, eff_data[not_missing,n,f,'rda',stat])
+}
+# Add labels
+if(f == 'all') axis(1)
+if(n == 'one2one') axis(2, las=1)
+if(f == 'none') mtext(topo_names[n], 3, 1, cex=.8)
+if(n == 'many2many'){
+	par(xpd=NA)
+	text(1, 1 , env_names[f], srt=-90, cex=1.3, adj=c(.5,-2))
+	par(xpd=F)
+}
+	
+# Add axis lines
+abline(h=par('usr')[3], lwd=3)
+abline(v=par('usr')[1], lwd=3)
+
+}} # closes for loops
+
+mtext(expression(Strength~~of~~mutualism~~(omega)), 1, 2.5, cex=.8, outer=T, adj=0.35)
+mtext('Std. mean difference', 2, 2.5, cex=.8, outer=T)
+
+plot.new()
+legend('center', cor_names, pch=cor_pch, bty='n', cex=1.2, title=expression(RDA~~R^2))
+
+dev.off()
+
+# Plot only for obligate mutualism
+jit = c(-.1,0,.1); names(jit) = topos
+x = 1:length(use_cor)
+
+svg(paste0(fig_dir,'Effects of obligate mutualism on community correlations across envfilt and topo (pooled).svg'), height=9, width=3.5)
+par(mfrow=c(4,1))
+par(mar=c(3,4,2,.5))
+par(lend=1)
+for(f in envfilt){
+	
+	plot(x, rep(0, length(use_cor)), type='n', xlim=c(0.5,length(use_cor)+.5),
+		ylim=range(CIs[,4,,,'rda',], na.rm=T),axes=F, ylab='Std. mean difference', xlab='')
+	
+	# Add axis lines
+	axis(1, at=x, labels=cor_names, tick=F, padj=0)
+	axis(2, las=1)
+	#abline(v=x+.5, lty=1, col='grey80')
+	abline(h=seq(-2,4,2), lty=2, col='grey')
+	abline(h=par('usr')[3], lwd=3)
+	abline(v=par('usr')[1], lwd=3)
+
+	# Add points
+	for(n in topos){
+		xvals = jit[n] + x
+		arrows(xvals, CIs[1,4,n,f,'rda',use_cor], xvals, CIs[3,4,n,f,'rda',use_cor], angle=90, length=.03, code=3)
+		points(xvals, eff_data[4,n,f,'rda',use_cor], pch=topo_pch[n])
+	}
+
+	# Add plot label
+	plot_lab = paste0(letters[which(envfilt==f)], ') ', env_names[f])
+	mtext(plot_lab, 3, at=-.5, line=0.5, adj=0, cex=.8)
+
+	# Add legend to first plot
+	if(f == 'none') legend('topright', topo_names[topos], pch=topo_pch[topos], border='grey', bg='white')
+
+}
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
